6a028436f283c4c6e675e8ba2a77f174d236406f
JimmyPinkard <jimmypinkard7@gmail.com>
2022-01-20 13:54:01 -0500

    Fixed BubbleSort

--- Strategy/Sorting/src/BubbleSort.java
+++ Strategy/Sorting/src/BubbleSort.java
@@ -11,19 +11,20 @@ public class BubbleSort implements SortBehavior {
      */
     @Override
     public ArrayList<String> sort(final ArrayList<String> data) {
+        final ArrayList<String> copy = new ArrayList<>(data);
         boolean hasSwapped;
         do {
             hasSwapped = false;
-            for(int i = 0; i < data.size() - 1; ++i) {
-                final String temp = data.get(i);
-                if(data.get(i).compareTo(data.get(i + 1)) > 0) {
-                    data.set(i, data.get(i + 1));
-                    data.set(i + 1, temp);
+            for(int i = 0; i < copy.size() - 1; ++i) {
+                final String temp = copy.get(i);
+                if(copy.get(i).compareTo(copy.get(i + 1)) > 0) {
+                    copy.set(i, copy.get(i + 1));
+                    copy.set(i + 1, temp);
                     hasSwapped = true;
                 }
             }
         }
         while(hasSwapped);
-        return data;
+        return copy;
     }
 }
